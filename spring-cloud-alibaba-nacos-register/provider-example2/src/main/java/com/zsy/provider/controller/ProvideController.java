773f89e17054fbcad729a50465dfbf3ddd359e4c
zsy <zsy4884@gmail.com>
2021-10-18 16:41:48 +0800

    提供第二个生产者，测试openfeign的负载均衡

---
+++ spring-cloud-alibaba-nacos-register/provider-example2/src/main/java/com/zsy/provider/controller/ProvideController.java
@@ -0,0 +1,20 @@
+package com.zsy.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zousy
+ * @version v1.0
+ * @Description
+ * @date 2021-10-14 9:58
+ */
+@RestController
+public class ProvideController {
+
+    @GetMapping("provide/{message}")
+    public String consume(@PathVariable String message){
+        return "hello nacos 2222" + message;
+    }
+}
